78b257aa43d0dd7100bf40c1cdf6f259958c535b
ancip <antoniusciptawijaya@yahoo.com>
2021-10-28 03:23:58 +0700

    fixing unworking javascript

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
 const display = document.getElementById('clock');
+const audio = new Audio('./assets/Ram-Ranch.mp3');
+audio.loop = true;
+let alarmTime = null;
+let alarmTimeout = null;
 
 function updateTime(){
-    const date = new date();
+    const date = new Date();
+    
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const hour = formatTime(date.getHours());
+    const minutes = formatTime(date.getMinutes());
+    const seconds = formatTime(date.getSeconds());
 
-    const hour = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-
+    document.getElementById('datetoday').innerHTML = days[date.getDay()]+","+date.getDate()+'-'+(date.getMonth()+1)+'-'+date.getYear();
     display.innerText=`${hour} : ${minutes} : ${seconds}`;
 }
+
+function formatTime(time){
+    if( time < 10 ){
+        return '0' + time;
+    }
+    return time;
+}
+
+function setAlarmTime(value){
+    alarmTime = value;
+    console.log(alarmTime);
+}
+
+function setAlarm(){
+    if(alarmTime){
+        const current = new Date();
+        const timeToAlarm = new Date(alarmTime);
+
+        const hour = formatTime(timeToAlarm.getHours() - current.getHours());
+        const minutes = formatTime(timeToAlarm.getMinutes() - current.getMinutes());
+
+        if(timeToAlarm > current){
+            const timeout = timeToAlarm.getTime() - current.getTime();
+            alarmTImeon = setTimeout(() => audio.play(), timeout);
+
+            if(hour < 1){
+                alert(`Alarm set for ${minutes} Minutes ! -Seagull`);
+            }else 
+                alert(`Alarm set for ${hour} Hours ${minutes} Minutes ! -Seagull`);
+        }
+    }
+}
+
+function clearAlarm(){
+    audio.pause();
+    audio.load();
+    if(alarmTimeout){
+        clearTimeout(alarmTimeout);
+    }
+    alert('Alarm Cleared. Good morning and Have a nice day! :) -Seagull');
+}
+
 setInterval(updateTime, 1000);
